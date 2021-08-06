7fcdb16b06165b03073794514891d976ae73be12
jhan diaz <jmauricio468@gmail.com>
2021-08-06 01:33:35 -0500

    se agregó la conexion de la db

---
+++ src/main/java/com/mycompany/juegodecarros/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.juegodecarros;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jhanm
+ */
+public class Conexion {
+     public Connection get_connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/juegodecarros", "root", "");
+           
+        } catch (SQLException e) {
+
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
